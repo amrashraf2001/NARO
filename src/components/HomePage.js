e9040b7ba9a8bc5894cd9b67bba002af76979f2d
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-09-03 01:13:37 +0300

    home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,8 +5,29 @@ import './HomePage.css'
 
 const HomePage = () => (
   
-  <div className="wuf-container">
-
+  <div className="afro-details">
+    <header className="afro-header">
+        <br />
+        <div className="afro-countdown">
+          <p>
+            <strong>About Us</strong>
+          </p>
+        </div>
+        <div className="header-content">
+          {/* <button className="button-64" role="button" >
+          <span className="text"><Link to="/NARO/about/vision" style={{ textDecoration: 'none', color: 'white' }}>Vision</Link></span>
+          </button>
+          <button className="button-64" role="button">
+            <span className="text"><Link to="/NARO/about/mission" style={{ textDecoration: 'none', color: 'white' }}>Mission</Link></span>
+          </button> */}
+        </div>
+        <div className="header-image">
+          <img
+            src="https://img.youm7.com/ArticleImgs/2019/6/19/97159-WhatsApp-Image-2019-06-19-at-5.35.27-PM-(1).jpeg"
+            alt="Africities Summit"
+          />
+        </div>
+      </header>
   </div>
   
 );
