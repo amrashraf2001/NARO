22505be1884a44de0c10d25b7d16a70438895b50
Amr Ashraf <102065583+amrashraf2001@users.noreply.github.com>
2024-09-02 15:04:24 +0300

    fix

--- src/components/MissionPage.js
+++ src/components/MissionPage.js
@@ -11,7 +11,9 @@ return (
                     <strong>Mission</strong>
                 </p>
             </div>
-            <div className="header-content">
+            <div className="header-image">
+                <img src="https://img.freepik.com/free-photo/3d-rendering-arrow-hitting-target_23-2151266523.jpg?t=st=1725234403~exp=1725238003~hmac=9781bb1ec0087de05733ab7d9eaf5fbdd76fe5cad6889ab1b4f08b1ef5096a84&w=740" alt="Africities Summit" />
+                <div className="header-content">
                 <br />
                 <p><strong>Unite North African Local Governments:</strong> Promote the unification of North African local governments within the broader context of regional political and economic development, ensuring their representation in all relevant forums.</p>
                 <p><strong>Establish Local Government as a Distinct Sphere:</strong> Advocate for the recognition of North African local governments as a distinct and integral sphere of governance, working in support of national and other governmental levels.</p>
@@ -19,9 +21,9 @@ return (
                 <p><strong>Promote Resource Sharing:</strong> Facilitate the sharing of resources, knowledge, and best practices among North African local governments, fostering collaboration and mutual support.</p>
                 <p><strong>Pursue Sustainable Development:</strong> Promote sustainable development in North Africa, advocating for policies and initiatives that address the region's social, economic, and environmental challenges.</p>
             </div>
-            <div className="header-image">
-                <img src="https://img.freepik.com/free-photo/3d-rendering-arrow-hitting-target_23-2151266523.jpg?t=st=1725234403~exp=1725238003~hmac=9781bb1ec0087de05733ab7d9eaf5fbdd76fe5cad6889ab1b4f08b1ef5096a84&w=740" alt="Africities Summit" />
             </div>
+            
+           
         </header>
     </div>
 );
